b8368db016eea57c9df6c3ceff3c6d55c274555d
Arham Raza <arhamraza399@gmail.com>
2023-09-17 10:31:11 +0500

    animation changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,15 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Reveal from "./Reveal";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
       {children}
-      <Footer />
+      <Reveal>
+        <Footer />
+      </Reveal>
     </>
   );
 }
