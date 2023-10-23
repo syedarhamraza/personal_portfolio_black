77f62dea603561cd6a34e71d1c31c3449f830649
Arham Raza <arhamraza399@gmail.com>
2023-10-23 20:27:03 +0500

    logo

--- pages/index.js
+++ pages/index.js
@@ -139,18 +139,6 @@ export default function Home() {
             sizes="180x180"
             href="/apple-touch-icon.png"
           />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
           <link rel="icon" href="/favicon.ico" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Syed Arham Raza" key="title" />
--- pages/links.js
+++ pages/links.js
@@ -17,18 +17,6 @@ export default function faq() {
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Syed Arham Raza" key="title" />
