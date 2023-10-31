11d954e9aed7e2a028b8be86c6ee11c7aa5ad727
Arham Raza <arhamraza399@gmail.com>
2023-10-31 18:55:37 +0500

    script removed

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 import Sidebar from "../components/sidebar/Sidebar";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
