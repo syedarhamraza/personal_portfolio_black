c3c84f7c702b456a3fe48cd65fe08e4b7b12589b
Arham Raza <arhamraza399@gmail.com>
2023-11-03 09:29:55 +0500

    fixed error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
-import Sidebar from "../components/sidebar/Sidebar";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,10 +12,6 @@ export default function App({ Component, pageProps }) {
         }}
       >
         <Component {...pageProps} />
-<<<<<<< HEAD
-=======
-        <Sidebar />
->>>>>>> 11d954e9aed7e2a028b8be86c6ee11c7aa5ad727
         <ScrollToTopButton />
       </MantineProvider>
     </>
